de15aba1908b07fce23552c6cc0ff53fa950f8e3
ChStLeGitHub <133281886+ChStLeGitHub@users.noreply.github.com>
2023-06-21 16:19:22 -0400

    Corrected directory

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-car_data = pd.read_csv('/Users/cslewicki/Desktop/Sprint4Project/vehicles_us.csv')
+car_data = pd.read_csv(“vehicles_us.csv”)
 
 price_data = px.histogram(car_data[car_data['price'] <= 50000],
                           color_discrete_sequence=['blue'],
